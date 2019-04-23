4d32915d35d075e6fb70f65f9e7acfb271797b9e
Белая Рысь <wlnx@users.noreply.github.com>
2019-04-23 14:19:41 +0300

    Remove AdjustPrivileges

--- rawcopy/rawcopy.cpp
+++ rawcopy/rawcopy.cpp
@@ -32,74 +32,74 @@ void ShowHelp() {
 	wprintf_s(L"\t6\tDestination file exists. Use flag /f to override.\n\n");
 }
 
-bool AdjustPrivileges(bool IsBackup = true, bool Internal = false) {
-	///<summary>Commonly used to store required size for system data.</summary>
-	DWORD Size = 0;
-	
-	///<summary>Shows if required privilege is enabled.</summary>
-	bool ReqPrivEnabled = false;
-	
-	///<summary>Current process token used to adjust privileges.</summary>
-	HANDLE hProcToken = NULL;
-	
-	if (OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY | TOKEN_ADJUST_PRIVILEGES, &hProcToken))
-	{
-		GetTokenInformation(hProcToken, TokenPrivileges, NULL, 0, &Size);
-		wprintf_s(L"Size requested: %d\n\n", Size);
-		PTOKEN_PRIVILEGES PrivInfo = (PTOKEN_PRIVILEGES)(malloc(Size));
-		if (GetTokenInformation(hProcToken, TokenPrivileges, PrivInfo, Size, &Size))
-		{
-			bool ReqPrivFound = false;
-			const wchar_t* ReqPrivName = IsBackup ? SE_BACKUP_NAME : SE_RESTORE_NAME;
-			for (size_t i = 0; i < PrivInfo->PrivilegeCount; i++)
-			{
-				Size = 0;
-				LookupPrivilegeName(NULL, &(PrivInfo->Privileges[i].Luid), NULL, &Size);
-				LPWSTR Name = new TCHAR[Size];
-				LookupPrivilegeName(NULL, &(PrivInfo->Privileges[i].Luid), Name, &Size);
-				if (!ReqPrivFound && !wcscmp(Name, ReqPrivName))
-				{
-					wprintf_s(L"Name: %s\nAttributes: %d\n\n", Name, PrivInfo->Privileges[i].Attributes);
-					ReqPrivFound = true;
-					ReqPrivEnabled = (bool)(PrivInfo->Privileges[i].Attributes & SE_PRIVILEGE_ENABLED);
-					if (ReqPrivEnabled)
-					{
-						wprintf_s(L"Required privilege is enabled.\n");
-					}
-					else
-					{
-						wprintf_s(L"Required privilege is disabled.\n");
-						if (!Internal)
-						{
-							PrivInfo->Privileges[i].Attributes |= SE_PRIVILEGE_ENABLED;
-							Size = 0;
-							AdjustTokenPrivileges(hProcToken, false, PrivInfo, 0, NULL, &Size);
-							wprintf_s(L"Log size requested: %d\n", Size);
-							PTOKEN_PRIVILEGES logPrivInfo = (PTOKEN_PRIVILEGES)(malloc(Size));
-							if (ERROR_SUCCESS == AdjustTokenPrivileges(hProcToken, false, PrivInfo, Size, logPrivInfo, &Size))
-							{
-								wprintf_s(L"Privileges adjusted. Rescanning...\n\n");
-								ReqPrivEnabled = AdjustPrivileges(IsBackup, true);
-							}
-						}
-					}
-					break;
-				}
-				wprintf_s(L"1\n");
-			}
-		}
-		else
-		{
-			fwprintf_s(stderr, L"Couldn't get process token information. Error code: %d", GetLastError());
-		}
-		CloseHandle(hProcToken);
-	}
-	else
-	{
-		fwprintf_s(stderr, L"Couldn't get process token. Error code: %d", GetLastError());
-	}
-	return ReqPrivEnabled;
-}
+//bool AdjustPrivileges(bool IsBackup = true, bool Internal = false) {
+//	///<summary>Commonly used to store required size for system data.</summary>
+//	DWORD Size = 0;
+//	
+//	///<summary>Shows if required privilege is enabled.</summary>
+//	bool ReqPrivEnabled = false;
+//	
+//	///<summary>Current process token used to adjust privileges.</summary>
+//	HANDLE hProcToken = NULL;
+//	
+//	if (OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY | TOKEN_ADJUST_PRIVILEGES, &hProcToken))
+//	{
+//		GetTokenInformation(hProcToken, TokenPrivileges, NULL, 0, &Size);
+//		wprintf_s(L"Size requested: %d\n\n", Size);
+//		PTOKEN_PRIVILEGES PrivInfo = (PTOKEN_PRIVILEGES)(malloc(Size));
+//		if (GetTokenInformation(hProcToken, TokenPrivileges, PrivInfo, Size, &Size))
+//		{
+//			bool ReqPrivFound = false;
+//			const wchar_t* ReqPrivName = IsBackup ? SE_BACKUP_NAME : SE_RESTORE_NAME;
+//			for (size_t i = 0; i < PrivInfo->PrivilegeCount; i++)
+//			{
+//				Size = 0;
+//				LookupPrivilegeName(NULL, &(PrivInfo->Privileges[i].Luid), NULL, &Size);
+//				LPWSTR Name = new TCHAR[Size];
+//				LookupPrivilegeName(NULL, &(PrivInfo->Privileges[i].Luid), Name, &Size);
+//				if (!ReqPrivFound && !wcscmp(Name, ReqPrivName))
+//				{
+//					wprintf_s(L"Name: %s\nAttributes: %d\n\n", Name, PrivInfo->Privileges[i].Attributes);
+//					ReqPrivFound = true;
+//					ReqPrivEnabled = (bool)(PrivInfo->Privileges[i].Attributes & SE_PRIVILEGE_ENABLED);
+//					if (ReqPrivEnabled)
+//					{
+//						wprintf_s(L"Required privilege is enabled.\n");
+//					}
+//					else
+//					{
+//						wprintf_s(L"Required privilege is disabled.\n");
+//						if (!Internal)
+//						{
+//							PrivInfo->Privileges[i].Attributes |= SE_PRIVILEGE_ENABLED;
+//							Size = 0;
+//							AdjustTokenPrivileges(hProcToken, false, PrivInfo, 0, NULL, &Size);
+//							wprintf_s(L"Log size requested: %d\n", Size);
+//							PTOKEN_PRIVILEGES logPrivInfo = (PTOKEN_PRIVILEGES)(malloc(Size));
+//							if (ERROR_SUCCESS == AdjustTokenPrivileges(hProcToken, false, PrivInfo, Size, logPrivInfo, &Size))
+//							{
+//								wprintf_s(L"Privileges adjusted. Rescanning...\n\n");
+//								ReqPrivEnabled = AdjustPrivileges(IsBackup, true);
+//							}
+//						}
+//					}
+//					break;
+//				}
+//				wprintf_s(L"1\n");
+//			}
+//		}
+//		else
+//		{
+//			fwprintf_s(stderr, L"Couldn't get process token information. Error code: %d", GetLastError());
+//		}
+//		CloseHandle(hProcToken);
+//	}
+//	else
+//	{
+//		fwprintf_s(stderr, L"Couldn't get process token. Error code: %d", GetLastError());
+//	}
+//	return ReqPrivEnabled;
+//}
 
 DWORD WINAPI PfeExportFunc(
 	PBYTE pbData,
@@ -108,6 +108,25 @@ DWORD WINAPI PfeExportFunc(
 )
 {
 	(reinterpret_cast<fstream*>(pvCallbackContext))->write((char*)pbData, ulLength);
+	return ERROR_SUCCESS;
+}
+
+DWORD WINAPI PfeImportFunc(
+	PBYTE pbData,
+	PVOID pvCallbackContext,
+	PULONG ulLength
+)
+{
+	fstream* file_raw = (reinterpret_cast<fstream*>(pvCallbackContext));
+	if (file_raw->eof())
+	{
+		*ulLength = 0;
+	}
+	else
+	{
+		file_raw->read((char*)pbData, *ulLength);
+		*ulLength = file_raw->gcount();
+	}
 	return ERROR_SUCCESS;
 }
 
@@ -160,7 +179,7 @@ int wmain(int argc, wchar_t* argv[])
 
 	bool IsBackup = (bool)(SrcFileAttrs & FILE_ATTRIBUTE_ENCRYPTED);
 
-	if (AdjustPrivileges(IsBackup))
+	if (/*AdjustPrivileges(IsBackup)*/true)
 	{
 		PVOID Context;
 		int rescode = 0;
@@ -187,11 +206,25 @@ int wmain(int argc, wchar_t* argv[])
 			fstream DstFile(DstFilePath, ios::app | ios::binary);
 			rescode = OpenEncryptedFileRaw(SrcFilePath.c_str(), 0, &Context);
 			if (!rescode) {
-				ReadEncryptedFileRaw(PfeExportFunc, (PVOID)(&DstFile), Context);
+				ReadEncryptedFileRaw(PfeExportFunc, &DstFile, Context);
 				CloseEncryptedFileRaw(Context);
 			}
 			DstFile.flush();
 			DstFile.close();
+		}
+		else
+		{
+			fstream SrcFile(SrcFilePath, ios::binary | ios::in);
+			rescode = OpenEncryptedFileRaw(DstFilePath.c_str(), CREATE_FOR_IMPORT, &Context);
+			if (ERROR_SUCCESS == rescode) {
+				WriteEncryptedFileRaw(PfeImportFunc, &SrcFile, Context);
+				CloseEncryptedFileRaw(Context);
+			}
+			else
+			{
+				fwprintf_s(stderr, L"Couldn't open file for restore: %s", DstFilePath.c_str());
+			}
+			SrcFile.close();
 		}
 		return rescode;
 	}
@@ -201,14 +234,3 @@ int wmain(int argc, wchar_t* argv[])
 	}
 	return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
